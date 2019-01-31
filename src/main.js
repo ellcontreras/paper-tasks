8c99f87632c5efb6c40b06fd8c6a069e3bb8e158
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-01-31 14:30:45 -0600

    Imported PaperCSS library

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
 
+import "papercss/dist/paper.min.css";
+
 Vue.config.productionTip = false;
 
 new Vue({
