8d178ca9520e3cf996a090caca1ca54ff77aca95
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-04 15:32:25 -0600

    Created component to display all tasks

---
+++ src/components/DisplayTasks.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card">
+    <div class="card-body">
+      <div v-for="(task, key) of $store.state.tasks" :key="key">
+        <h3>{{ task.title}}</h3>
+        <p>{{ task.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisplayTasks"
+};
+</script>
+
