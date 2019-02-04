3112e2fc9fe545fb712cc092d6281411b524ed38
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-02-04 15:21:48 -0600

    Setup the store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,27 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersist from "vuex-persist";
 
 Vue.use(Vuex);
 
+const vuexPersist = new VuexPersist({
+  key: "paper-tasks",
+  storage: localStorage
+});
+
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    tasks: []
+  },
+  mutations: {
+    addTask(state, payload) {
+      state.tasks.push(payload);
+    }
+  },
+  actions: {
+    ADD_TASK(context, payload) {
+      context.commit("addTask", payload);
+    }
+  },
+  plugins: [vuexPersist.plugin]
 });
