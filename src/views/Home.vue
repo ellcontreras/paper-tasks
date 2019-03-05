5cad27cecaf44bae57d8c49ec976d31ead290b1f
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-05 14:59:31 -0600

    Set up the Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,23 @@
 <template>
-  <div>
-    <h1>Hola mundo!</h1>
+  <div class="row">
+    <div class="col-8">
+      <display-tasks/>
+    </div>
+    <div class="col-4">
+      <form-add-task/>
+    </div>
   </div>
 </template>
 
 <script>
+import FormAddTask from "@/components/FormAddTask.vue";
+import DisplayTasks from "@/components/DisplayTasks.vue";
+
 export default {
-  name: "home"
+  name: "home",
+  components: {
+    FormAddTask,
+    DisplayTasks
+  }
 };
 </script>
