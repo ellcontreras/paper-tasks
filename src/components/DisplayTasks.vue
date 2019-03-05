b0b6175491f3e6ed36e373405d9b344a2e3829b0
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-05 14:59:18 -0600

    Ready the template to display each task and delete button is working

--- src/components/DisplayTasks.vue
+++ src/components/DisplayTasks.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <div v-for="(task, key) of $store.state.tasks" :key="key">
-        <h3>{{ task.title}}</h3>
-        <p>{{ task.description }}</p>
+      <div v-if="$store.state.tasks.length === 0">
+        <h3>Aún no hay tareas</h3>
       </div>
+      <article class="article" v-for="(task, key) of $store.state.tasks" :key="key">
+        <h3 class="article-title">{{ task.title}}</h3>
+        <p class="text-lead">{{ task.description }}</p>
+        <button class="btn-danger" @click="DeleteTask(task)">Eliminar</button>
+        <button class="btn-warning">Actualizar</button>
+      </article>
     </div>
   </div>
 </template>
 
 <script>
+import { DELETE_TASK } from "@/store/mutation-types";
+
 export default {
-  name: "DisplayTasks"
+  name: "DisplayTasks",
+  methods: {
+    DeleteTask(task) {
+      this.$store.commit(DELETE_TASK, task);
+    }
+  }
 };
 </script>
-
