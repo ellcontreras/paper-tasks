566811838aaffcc30f323b27ba61d17deef5464d
Luis Contreras Guerra <luisguerra495@gmail.com>
2019-03-05 14:43:16 -0600

    Imported toastr to the project

--- src/main.js
+++ src/main.js
@@ -1,16 +1,24 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import store from "./store/store";
 import "./registerServiceWorker";
 
 import "papercss/dist/paper.min.css";
+import "toastr/build/toastr.min.css";
 
 import VeeValidate from "vee-validate";
+import toastr from "toastr";
 
 Vue.use(VeeValidate);
 
 Vue.config.productionTip = false;
+
+toastr.options = {
+  positionClass: "toast-top-right"
+};
+
+Vue.prototype.$toastr = toastr;
 
 new Vue({
   router,
